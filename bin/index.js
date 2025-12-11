6e8d1325a5e3ea7fbffa326dbefcb00816159c65
Joshua Byrd <phocks@gmail.com>
2025-12-11 14:00:27 +1000

    Create hello world

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log('\n🎉 Hello from ABC News Create!\n');
+console.log('Welcome to the ABC News project initializer.');
+console.log('\nThis is a placeholder. Check back soon for more features!\n');
